f6142d00fe6206b893958594d7558dc709a07ffc
Samudra Gupta <113692208+nayansamudra@users.noreply.github.com>
2023-03-06 17:39:45 +0530

    Add files via upload

--- js/Form_validation.js
+++ js/Form_validation.js
@@ -359,7 +359,7 @@ $(document).ready(function () {
     })
         .on('hidden.bs.modal', function () {
             $('html').attr('style', 'overflow-x:hidden !important; overflow-y:auto !important')
-            $('body').attr('style', 'overflow-x:hidden !important; overflow-y:auto !important')
+            // $('body').attr('style', 'overflow-x:hidden !important; overflow-y:auto !important')
             $('body').css('overflow-x', 'clip')
         })
 
@@ -383,7 +383,7 @@ $(document).ready(function () {
         .on('hidden.bs.modal', function () {
             console.log('tredcode_modal is close')
             $('html').attr('style', 'overflow-x:hidden !important; overflow-y:auto !important')
-            $('body').attr('style', 'overflow-x:hidden !important; overflow-y:auto !important')
+            // $('body').attr('style', 'overflow-x:hidden !important; overflow-y:auto !important')
             $('body').css('overflow-x', 'clip')
         })
 });
